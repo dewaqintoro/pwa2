bb097ade2fb06ebdd1bcf2610e0f81ee6d0fbc5c
dewaqintoro <dewaqintoro@gmail.com>
2019-11-02 13:02:04 +0700

    caceng kluget kluget

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "c2";
+const CACHE_NAME = "c5";
 var urlsToCache = ["/", "/index.html", "/js/main.js", "/img/logo.png"];
  
 self.addEventListener("install", function(event) {
@@ -12,21 +12,85 @@ self.addEventListener("install", function(event) {
   );
 });
 
-self.addEventListener("fetch", function(event){
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      console.log("ServiceWorker : Menarik data : ", event.request.url)
+// self.addEventListener('fetch', function(event){
+//   event.respondWith(
+//     caches.match(event.request,{cacheName:CACHE_NAME})
+//     .then(function(response){
+//       if(response){
+//         return response;
+//       }
 
-      if(response){
-        console.log("serviceWorker: Gunakan aset dari cache : ", response.url)
+//       var fetchRequest = event.request.clone();
+
+//       return fetch(fetchRequest).then(
+//         function(response){
+//           if(!response || response.status !== 200){
+//             return response;
+//           }
+
+//           var responseToChace = response.clone();
+//           caches.open(CACHE_NAME)
+//           .then(function(cache){
+//             cache.put(event.request, responseToChace);
+//           });
+//           return Response;
+//         }
+//       );
+//     })
+//   );
+// });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request, {cacheName:CACHE_NAME})
+    .then(function(response) {
+      if (response) {
         return response;
       }
-
-      console.log(
-        "ServiceWorker : Memuat aset dari server",
-        event.request.url
-      )
-      return fetch(event.request)
+      var fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(
+        function(response) {
+          if(!response || response.status !== 200) {
+            return response;
+          }
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        }
+      );
     })
-  )
-})
+  );
+});
+
+// self.addEventListener('activate', function(event){
+//   console.log('aktivasi service worker baru');
+
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames){
+//       return Promise.all(
+//         cacheNames.map(function(cacheName){
+//           if(cacheName !== CACHE_NAME && cacheName.startsWith("dewa qintoro"))
+//             return caches.delete(cacheName);
+//         })
+//       )
+//     })
+//   )
+// })
+
+self.addEventListener('activate', function(event) {
+  console.log('Aktivasi service worker baru');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME && cacheName.startsWith("codepolitan-reader-lite")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
